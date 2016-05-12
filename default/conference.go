13017e4cf66eb5f68b4d0e90f848c19a720517a0
gitrambo <gitrambo@users.noreply.github.com>
2016-05-12 14:56:05 -0700

    Add files via upload
    initial git upload

---
+++ default/conference.go
@@ -0,0 +1,105 @@
+package main
+
+/*
+conference.go -- server-side Go App Engine API;
+    uses Google Cloud Endpoints
+
+*/
+
+
+import (
+	"log"
+	"github.com/GoogleCloudPlatform/go-endpoints/endpoints"
+	"net/http"
+)
+
+type ConferenceApi struct {
+}
+
+func copyProfileToForm(r *http.Request, prof *Profile) (*ProfileForm, error) {
+	//Copy relevant fields from Profile to ProfileForm.
+	pf := &ProfileForm{
+			DisplayName: prof.DisplayName,
+			MainEmail: prof.MainEmail,
+			TeeShirtSize: StringEnumToTeeShirtSize(prof.TeeShirtSize),
+	}
+	return pf, nil
+}
+
+func getProfileFromUser(r *http.Request) (*Profile, error) {
+	//Return user Profile from datastore, creating new one if non-existent.
+	//TODO
+	//make sure user is authed
+	//c := endpoints.NewContext(r)
+	//user, err := endpoints.CurrentUser(c, []string{endpoints.EmailScope},
+	//	[]string{WEB_CLIENT_ID, endpoints.APIExplorerClientID}, []string{WEB_CLIENT_ID, endpoints.APIExplorerClientID})
+	//if err != nil {
+	//	return nil, err
+	//}
+	//if user == nil {
+	//	return nil, endpoints.UnauthorizedError
+	//}
+	var profile *Profile
+	if profile == nil {
+		profile = &Profile{
+			DisplayName: "Test",
+			MainEmail: "",
+			TeeShirtSize: TeeShirtSizeToStringEnum(NOT_SPECIFIED),
+		}
+		//TODO
+	}
+	return profile, nil
+}
+
+func doProfile(r *http.Request, saveRequest *ProfileMiniForm) (*ProfileForm, error) {
+	//Get user Profile and return to user, possibly updating it first.
+	//get user Profile
+	prof, err := getProfileFromUser(r)
+	if err != nil {
+		return nil, err
+	}
+	
+	//if saveProfile(), process user-modifyable fields
+	if saveRequest != nil {
+		prof.TeeShirtSize = TeeShirtSizeToStringEnum(saveRequest.TeeShirtSize)
+		prof.DisplayName = saveRequest.DisplayName
+	}
+	
+	//return ProfileForm
+	return copyProfileToForm(r, prof)
+}
+
+func (h *ConferenceApi) GetProfile(r *http.Request) (*ProfileForm, error) {
+	//Return user profile.
+	return doProfile(r, nil)
+}
+
+func (h *ConferenceApi) SaveProfile(r *http.Request) (*ProfileForm, error) {
+	//Update & return user profile.
+	return doProfile(r, nil)
+}
+
+func init() {
+	//Conference API v0.1
+	conference := &ConferenceApi{}
+	//registers API
+	api, err := endpoints.RegisterService(conference, "conference", "v1", "Conference API", true)
+	if err != nil {
+		log.Fatalf("Register service: %v", err)
+	}
+	
+	register := func(orig, name, method, path, desc string) {
+		m := api.MethodByName(orig)
+		if m == nil {
+			log.Fatalf("Missing method %s", orig)
+		}
+		i := m.Info()
+		i.Name, i.HTTPMethod, i.Path, i.Desc = name, method, path, desc
+		i.Scopes = []string{endpoints.EmailScope}
+		i.ClientIds = []string{WEB_CLIENT_ID, endpoints.APIExplorerClientID}
+	}
+
+	register("GetProfile", "getProfile", "GET", "profile", "Get profile")
+	register("SaveProfile", "saveProfile", "POST", "profile", "Save profile")
+	endpoints.HandleHTTP()
+}
---
+++ solutions/REF_09_models.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type Profile struct {
+	//Profile -- User profile object
+	DisplayName string	`json:"displayName"`
+	MainEmail string	`json:"mainEmail"`
+	TeeShirtSize string	`json:"teeShirtSize"`
+	ConferenceKeysToAttend []string	`json:"conferenceKeysToAttend"`
+}
+
+type ProfileMiniForm struct {
+	//ProfileMiniForm -- update Profile form message
+	DisplayName string	`json:"displayName"`
+	TeeShirtSize TeeShirtSize	`json:"teeShirtSize"`
+}
+
+type ProfileForm struct {
+	//ProfileForm -- Profile outbound form message
+	DisplayName  string	`json:"displayName"`
+	MainEmail string	`json:"mainEmail"`
+	TeeShirtSize TeeShirtSize	`json:"teeShirtSize"`
+	ConferenceKeysToAttend []string	`json:"conferenceKeysToAttend"`
+}
+
+type StringMessage struct {
+	//StringMessage -- outbound (single) string message
+	Data string `json:"data"`
+}
+
+type BooleanMessage struct {
+	//BooleanMessage -- outbound Boolean value message
+	Data bool `json:"data"`
+}
+
+type Conference struct {
+	//Conference -- Conference object
+	Name string `json:"name"`
+	Description string `json:"description"`
+	OrganizerUserId string `json:"organizerUserId"`
+	Topics []string `json:"topics"`
+	City string `json:"city"`
+	StartDate time.Time `json:"startDate"`
+	Month int `json:"month"`
+	EndDate time.Time `json:"endDate"`
+	MaxAttendees int `json:"maxAttendees"`
+	SeatsAvailable int `json:"seatsAvailable"`
+}
+
+type ConferenceForm struct {
+	//ConferenceForm -- Conference outbound form message
+	Name string `json:"name"`
+	Description string `json:"description"`
+	OrganizerUserId string `json:"organizerUserId"`
+	Topics []string `json:"topics"`
+	City string `json:"city"`
+	StartDate string `json:"startDate"`
+	Month int `json:"month"`
+	MaxAttendees int `json:"maxAttendees,string,omitempty"`
+	SeatsAvailable int `json:"seatsAvailable"`
+	EndDate string `json:"endDate"`
+	WebsafeKey string `json:"websafeKey"`
+	OrganizerDisplayName string `json:"organizerDisplayName"`
+}
+
+type ConferenceForms struct {
+	//ConferenceForms -- multiple Conference outbound form message
+	Items []ConferenceForm `json:"items"`
+}
+
+type ConferenceQueryForm struct {
+	//ConferenceQueryForm -- Conference query inbound form message
+	Field string `json:"field"`
+	Operator string `json:"operator"`
+	Value string `json:"value"`
+}
+
+type ConferenceQueryForms struct {
+	//ConferenceQueryForms -- multiple ConferenceQueryForm inbound form message
+	Filters []ConferenceQueryForm `json:"filters"`
+}
+
+type TeeShirtSize int
+
+const (
+	//TeeShirtSize -- t-shirt size enumeration value
+	NOT_SPECIFIED TeeShirtSize = iota
+	XS_M
+	XS_W
+	S_M
+	S_W
+	M_M
+	M_W
+	L_M
+	L_W
+	XL_M
+	XL_W
+	XXL_M
+	XXL_W
+	XXXL_M
+	XXXL_W
+)
+
+var teeShirtSizeEnumTypeNames = map[TeeShirtSize]string{
+	NOT_SPECIFIED: "NOT_SPECIFIED",
+	XS_M: "XS_M",
+	XS_W: "XS_W",
+	S_M: "S_M",
+	S_W: "S_W",
+	M_M: "M_M",
+	M_W: "M_W",
+	L_M: "L_M",
+	L_W: "L_W",
+	XL_M: "XL_M",
+	XL_W: "XL_W",
+	XXL_M: "XXL_M",
+	XXL_W: "XXL_W",
+	XXXL_M: "XXXL_M",
+	XXXL_W: "XXXL_W",
+}
+
+func (m TeeShirtSize) MarshalJSON() ([]byte, error){
+	str := fmt.Sprintf("\"%s\"", TeeShirtSizeToStringEnum(m))
+    return []byte(str), nil
+}
+
+func (m *TeeShirtSize) UnmarshalJSON(value []byte) error {
+	str := strings.Replace(string(value), "\"", "", -1)
+	*m = StringEnumToTeeShirtSize(str)
+	return nil
+}
+
+func StringEnumToTeeShirtSize(enum string) TeeShirtSize {
+	for i, r := range teeShirtSizeEnumTypeNames {
+		if enum == r {
+			return TeeShirtSize(i)
+		}
+	}
+	return NOT_SPECIFIED
+}
+
+func TeeShirtSizeToStringEnum(key TeeShirtSize) string {
+	if val, ok := teeShirtSizeEnumTypeNames[key]; ok {
+		return val
+	}
+	return teeShirtSizeEnumTypeNames[NOT_SPECIFIED]
+}
+
+type Alert struct {
+	//Alert -- Alert object
+	Author  string `datastore:"author"`
+	Content string `datastore:"content"`
+	Date time.Time `datastore:"date"`
+}
+
+type LatestAlert struct {
+	//LatestAlert -- Latest alert message
+	Content string `json:"content"`
+}
