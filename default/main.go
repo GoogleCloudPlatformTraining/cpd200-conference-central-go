dc7bb41fbe3dadc7768f4f3ed020261124030a40
gitrambo <gitrambo@users.noreply.github.com>
2016-06-28 09:21:20 -0700

    2nd update in 3

--- default/main.go
+++ default/main.go
@@ -3,7 +3,22 @@ package main
 import (
 )
 
+func SetAnnouncementHandler(w http.ResponseWriter, r *http.Request) {
+	//Set Announcement in Memcache.
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	header := r.Header.Get("X-AppEngine-Cron")
+	if header == "" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("attempt to access cron handler directly, missing custom App Engine header"))
+		return
+	}
+	cacheAnnouncement(r)
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func init() {
 	http.HandleFunc("/crons/set_announcement", SetAnnouncementHandler)
-	http.HandleFunc("/tasks/send_confirmation_email", SendConfirmationEmailHandler)
 }
